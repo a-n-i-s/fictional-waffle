0d58cd39e4838224133cacbf1a85b19961a6e994
a-n-i-s <anisur.ra7man@gmail.com>
2023-10-29 01:47:50 -0400

    feature: added rating and enquiry

--- apps/enquiries/views.py
+++ apps/enquiries/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from real_estate.settings.development import DEFAULT_FROM_EMAIL
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.views import Response
+
+from .models import EnQuiry
+
+
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def send_enquiry_email(request):
+    data = request["data"]
+    try:
+        subject = data["subject"]
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        from_email = data["email"]
+        recipient_list = data[DEFAULT_FROM_EMAIL]
+
+        send_mail(suject, message, from_email, recipient_list, fail_silently=True)
+
+        enquiry = EnQuiry(name=name, subject=subject, email=email, message=message)
+        enquiry.save()
+
+        return Response({"success": "Your enquiry was successfully submitted"})
+    except:
+        return Response({"fail": "Your enquiry was not sent. Please try again"})
--- apps/ratings/views.py
+++ apps/ratings/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.views import Response
 
-# Create your views here.
+from apps.profiles.models import Profile
+from .models import Rating
+
+User = get_user_model()
+
+
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def create_agent_review(request, profile_id):
+    agent_profile = Profile.objects.get(id=profile_id, is_agent=True)
+    data = request.data
+
+    profile_user = User.objects.get(pkid=agent_profile.user.pkid)
+    if profile_user.email == request.user.email:
+        return Response(
+            {"message": "You cannot rate yourself"}, status=status.HTTP_403_FORBIDDEN
+        )
+
+    if agent_profile.agent_review.filter(agent_pkid=request.user.pkid):
+        return Response(
+            {"message": "Profile allready reviewed"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    elif data["rating"] == 0:
+        return Response(
+            {"message": "Please select a review"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    else:
+        review = Rating.objects.create(
+            rater=request.user,
+            agent=agent_profile,
+            rating=data["rating"],
+            comment=data["comment"],
+        )
+
+        return Response("Review added")
